c23d3a8b6038df364c894a4907f4436ecd7965e9
piajoelle <eggimann.pia@gmail.com>
2017-07-21 17:09:51 +0200

    Initial commit

---
+++ Selbtest Vorkurs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by piajoelle on 15.07.17.
+ */
+
+function zaehleRueckwarts (zahl) {
+    for (let i=zahl; i === 0; i--)
+        console.log(i)
+}
+
+zeahleRueckwarts(3);
---
+++ WeitergehtderSpassmitKusi.js
@@ -0,0 +1,8 @@
+/**
+ * Created by piajoelle on 21.07.17.
+ */
+var ARR = [2, -1, 4, -4, 8, -8, 16, -16];
+
+for (var i = [0]; i < ARR.length; i++) {
+    console.log(ARR[i]);
+}
