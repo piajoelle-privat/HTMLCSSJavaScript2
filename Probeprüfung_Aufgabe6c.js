9326a97837bd8b73d1d421e61fdc61c20360b6eb
piajoelle <eggimann.pia@gmail.com>
2017-07-22 16:09:21 +0200

    random

--- Probeprüfung_Aufgabe6c.js
+++ Probeprüfung_Aufgabe6c.js
@@ -1,3 +1,15 @@
-/**
- * Created by piajoelle on 21.07.17.
- */
+
+var ARR = [1, -1, 4, -4, 8, -8, 16, -16];
+var DurchschnittPositiveZahlen = 0;
+
+for (var i = 0; i < ARR.length; i++) {
+    if (ARR[i] > 0) DurchschnittPositiveZahlen = DurchschnittPositiveZahlen + ARR[i]
+}
+
+console.log(DurchschnittPositiveZahlen);
+
+DurchschnittPositiveZahlen = 29/4;
+
+for (var e = 0; e < ARR.length; e++) if (ARR[e] > DurchschnittPositiveZahlen) {
+    console.log(ARR[e])
+}
